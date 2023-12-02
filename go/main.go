78700cdc278808b61cd9088b10274b8bfa8b8da8
Hrvoje Lesar <hrvoje.lesar1@hotmail.com>
2023-12-02 21:20:16 +0100

    Reading website sections and section content from files.

--- go/main.go
+++ go/main.go
@@ -1,21 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
+	"io"
+	"log"
 	"net/http"
+	"os"
+	"sort"
 )
 
 type Section struct {
-	Title string `json:"Title"`
+	Title       string `json:"Title"`
+	Subsections []Subsection
 }
 
 type Subsection struct {
+	Title     SubsectionTitle  `json:"title"`
 	ShortText *string          `json:"short_text"`
 	MainText  *string          `json:"main_text"`
 	NoteText  *string          `json:"note_text"`
-	Links     []SubsectionLink `json:"links"`
 	Image     *string          `json:"image"`
+	Links     []SubsectionLink `json:"links"`
 }
 
 type SubsectionTitle struct {
@@ -29,9 +36,81 @@ type TemplateData struct {
 }
 
 type SubsectionLink struct {
-	Title string `json:"title"`
-	Text  string `json:"text"`
-	Icon  string `json:"icon"`
+	Title string  `json:"title"`
+	Text  string  `json:"text"`
+	Url   *string `json:"url"`
+	Icon  string  `json:"icon"`
+}
+
+func readSections() []Section {
+	sections := []Section{}
+	fileBytes, err := readWebsiteDocsFile("website_docs/sections.json")
+	if err != nil {
+		log.Println(err)
+		return sections
+	}
+
+	err = json.Unmarshal(fileBytes, &sections)
+	if err != nil {
+		log.Println(err)
+		return sections
+	}
+
+	return sections
+}
+
+func readWebsiteDocsFile(filePath string) ([]byte, error) {
+	jsonFile, err := os.Open(filePath)
+	defer jsonFile.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	fileBytes, err := io.ReadAll(jsonFile)
+	if err != nil {
+		return nil, err
+	}
+
+	return fileBytes, nil
+}
+
+func readSubsections(sections []Section) []Section {
+	for idx := range sections {
+		section := &sections[idx]
+		section.Subsections = make([]Subsection, 0)
+		sectionDirPath := fmt.Sprintf("website_docs/%s", section.Title)
+		sectionDir, err := os.Open(sectionDirPath)
+		defer sectionDir.Close()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		files, err := sectionDir.ReadDir(0)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		sort.SliceStable(files, func(i, j int) bool {
+			return files[i].Name() < files[j].Name()
+		})
+		for _, file := range files {
+			filePath := fmt.Sprintf("%s/%s", sectionDirPath, file.Name())
+			fileBytes, err := readWebsiteDocsFile(filePath)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			subsection := Subsection{}
+			err = json.Unmarshal(fileBytes, &subsection)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			section.Subsections = append(section.Subsections, subsection)
+		}
+	}
+
+	return sections
 }
 
 func main() {
