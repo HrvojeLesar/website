8a795398a8202bb1d78f872af541649ce2685131
Hrvoje Lesar <hrvoje.lesar1@hotmail.com>
2026-03-27 11:36:56 +0100

    Removed unused react code

--- src/Components/Divider.tsx
+++
@@ -1,7 +0,0 @@
-export default function Divider() {
-    return (
-        <div className="flex py-2 items-center">
-            <div className="flex-grow border-t-2 rounded border-gray-400" />
-        </div>
-    );
-}
